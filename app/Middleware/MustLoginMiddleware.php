1cf11d0c23464456ac5e92740e8466fc6a10e37a
Ahmad Jaelani <ahmadjaelani8685@gmail.com>
2024-02-28 19:18:22 +0700

    Add Auth Middleware

---
+++ app/Middleware/MustLoginMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace RootNameSpace\Belajar\PHP\MVC\Middleware;
+
+use RootNameSpace\Belajar\PHP\MVC\App\View;
+use RootNameSpace\Belajar\PHP\MVC\Config\Database;
+use RootNameSpace\Belajar\PHP\MVC\Repository\SessionRepository;
+use RootNameSpace\Belajar\PHP\MVC\Repository\UserRepository;
+use RootNameSpace\Belajar\PHP\MVC\Service\SessionService;
+
+class MustLoginMiddleware implements Middleware
+{
+    private SessionService $sessionService;
+
+    public function __construct()
+    {
+        $sessionRepository = new SessionRepository(Database::getConnection());
+        $userRepository = new UserRepository(Database::getConnection());
+        $this->sessionService = new SessionService($sessionRepository, $userRepository);
+    }
+
+    function before(): void
+    {
+        $user = $this->sessionService->current();
+        if ($user == null) {
+            View::redirect('/users/login');
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -6,25 +6,23 @@ use RootNameSpace\Belajar\PHP\MVC\Controller\AuthorController;
 use RootNameSpace\Belajar\PHP\MVC\Controller\BooksController;
 use RootNameSpace\Belajar\PHP\MVC\Controller\ProductController;
 use RootNameSpace\Belajar\PHP\MVC\Controller\UsersController;
-
+use RootNameSpace\Belajar\PHP\MVC\Middleware\MustLoginMiddleware;
+use RootNameSpace\Belajar\PHP\MVC\Middleware\MustNotLoginMiddleware;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-Router::add('GET', '/users/register', UsersController::class, 'register');
-Router::add('POST', '/users/register', UsersController::class, 'postRegister');
-Router::add('GET', '/users/login', UsersController::class, 'login');
-Router::add('POST', '/users/login', UsersController::class, 'postLogin');
+Router::add('GET', '/users/register', UsersController::class, 'register', [MustNotLoginMiddleware::class]);
+Router::add('POST', '/users/register', UsersController::class, 'postRegister', [MustNotLoginMiddleware::class]);
+Router::add('GET', '/users/login', UsersController::class, 'login',  [MustNotLoginMiddleware::class]);
+Router::add('POST', '/users/login', UsersController::class, 'postLogin',  [MustNotLoginMiddleware::class]);
 
+Router::add('GET', '/author', AuthorController::class, 'author', [MustLoginMiddleware::class]);
 
-
-
-Router::add('GET', '/author', AuthorController::class, 'author');
-
-Router::add('GET', '/', BooksController::class, 'dashboard');
-Router::add('GET', '/remove', BooksController::class, 'removeBook');
-Router::add('GET', '/add-book', BooksController::class, 'viewAddBook');
-Router::add('GET', '/add-book/([0-9a-zA-Z]*)', BooksController::class, 'viewAddBook');
-Router::add('POST', '/add-book', BooksController::class, 'postAddBook');
-Router::add('GET', '/register', BooksController::class, 'register');
+Router::add('GET', '/', BooksController::class, 'dashboard', [MustLoginMiddleware::class]);
+Router::add('GET', '/remove', BooksController::class, 'removeBook', [MustLoginMiddleware::class]);
+Router::add('GET', '/add-book', BooksController::class, 'viewAddBook', [MustLoginMiddleware::class]);
+Router::add('GET', '/add-book/([0-9a-zA-Z]*)', BooksController::class, 'viewAddBook', [MustLoginMiddleware::class]);
+Router::add('POST', '/add-book', BooksController::class, 'postAddBook', [MustLoginMiddleware::class]);
+Router::add('GET', '/register', BooksController::class, 'register', [MustLoginMiddleware::class]);
 
 Router::run();
